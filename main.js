4d45d23d792d98ef353fa7c2515dde619673f040
ahmetys <66472682+ahmetys@users.noreply.github.com>
2025-05-16 19:50:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,20 +5,30 @@ if ('serviceWorker' in navigator) {
 }
 
 function subscribe() {
-  Notification.requestPermission().then(permission => {
-    if (permission === 'granted') {
-      navigator.serviceWorker.getRegistration().then(reg => {
-        reg.showNotification('Merhaba!', {
-          body: 'Bu bir PWA bildirimidir.',
-          icon: 'icon-192.png'
-        });
+  if (Notification.permission === "granted") {
+    navigator.serviceWorker.getRegistration().then(reg => {
+      reg.showNotification('Merhaba!', {
+        body: 'Bu bir olay sonrası tetiklenen bildirimi!',
+        icon: 'icon-192.png'
       });
+    });
+  }
+}
+
+function veriGeldigindeBildirim() {
+  console.log("Veri alınıyor...");
+  setTimeout(() => {
+    console.log("Veri geldi!");
+    subscribe();
+  }, 3000);
+}
+
+function izinIste() {
+  Notification.requestPermission().then(permission => {
+    if (permission === "granted") {
+      alert("Bildirim izni verildi!");
     } else {
-      alert('Bildirim izni verilmedi.');
+      alert("Bildirim izni verilmedi.");
     }
   });
 }
-
-setTimeout(()=>{
-subscribe()
-},10000)
